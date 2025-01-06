5732c3d3365ff3b9c40a38becaf2654b8c171331
Marcos Cabrera <marcos04cabrera@gmail.com>
2025-01-06 15:15:33 -0400

    feat: tech router outlet

---
+++ src/app/pages/tech/dashboard-tech/dashboard-tech.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-tech',
+  templateUrl: './dashboard-tech.component.html',
+  styleUrls: ['./dashboard-tech.component.scss'],
+})
+export class DashboardTechComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
