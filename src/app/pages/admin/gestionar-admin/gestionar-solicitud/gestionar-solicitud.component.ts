a93dcf189871ca2a6fc287b210f4547610cb27da
Josmar <josmarreyes9875@gmail.com>
2025-01-18 01:57:33 -0400

    Estructura Carpetas Admin

---
+++ src/app/pages/admin/gestionar-admin/gestionar-solicitud/gestionar-solicitud.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestionar-solicitud',
+  templateUrl: './gestionar-solicitud.component.html',
+  styleUrls: ['./gestionar-solicitud.component.scss'],
+})
+export class GestionarSolicitudComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/admin/home-admin/home-admin.page.ts
+++ src/app/pages/admin/home-admin/home-admin.page.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavbarComponent } from "../../../shared/components/sidebar/sidebar.component";
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.page.html',
   styleUrls: ['./home-admin.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent, RouterOutlet, RouterLink]
 })
 export class HomeAdminPage implements OnInit {
 
