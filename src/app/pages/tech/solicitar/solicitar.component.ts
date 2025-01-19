74198f3be049c28a05721edb942e26a459c48c59
shinso-raven <1116252@est.intec.edu.do>
2025-01-19 17:45:07 -0400

    style: maqueta solicitar servicio

--- src/app/pages/tech/solicitar/solicitar.component.ts
+++ src/app/pages/tech/solicitar/solicitar.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
 import { HomeButtonComponent } from '../../../shared/components/home-button/home-button.component';
 import { SidebarButtonComponent } from '../../../shared/components/sidebar-button/sidebar-button.component';
+import { ToggleElevatorSectionComponent } from '../../../shared/components/toggle-elevator-section/toggle-elevator-section.component';
 
 @Component({
   selector: 'app-solicitar',
   templateUrl: './solicitar.component.html',
   styleUrls: ['./solicitar.component.scss'],
-  imports: [SidebarButtonComponent, HomeButtonComponent],
+  imports: [
+    SidebarButtonComponent,
+    HomeButtonComponent,
+    IonIcon,
+    ToggleElevatorSectionComponent,
+  ],
 })
 export class SolicitarComponent implements OnInit {
-  constructor() {}
+  AddService() {
+    throw new Error('Method not implemented.');
+  }
+  SendRequest() {
+    throw new Error('Method not implemented.');
+  }
+  OpenRequest() {
+    throw new Error('Method not implemented.');
+  }
+  solicitudes = [];
+
+  constructor() {
+    addIcons({
+      expand: '/assets/icon/expand-action.svg',
+      plus: '/assets/icon/plus.svg',
+    });
+  }
 
   ngOnInit() {}
+
+  ChangeSectionType($event: string) {
+    console.log('Section type: ' + $event);
+  }
 }
