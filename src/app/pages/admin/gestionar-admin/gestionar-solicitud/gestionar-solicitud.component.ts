ad2e8416e729996592da542b5c977730b36fba40
Josmar <josmarreyes9875@gmail.com>
2025-01-19 19:39:27 -0400

    Paginas gestionar(incompleto)

--- src/app/pages/admin/gestionar-admin/gestionar-solicitud/gestionar-solicitud.component.ts
+++ src/app/pages/admin/gestionar-admin/gestionar-solicitud/gestionar-solicitud.component.ts
@@ -1,14 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
+interface Request {
+  name: string;
+  email: string;
+  status: string;
+  role: string;
+  relatedTeam: string;
+}
 @Component({
   selector: 'app-gestionar-solicitud',
   templateUrl: './gestionar-solicitud.component.html',
   styleUrls: ['./gestionar-solicitud.component.scss'],
+  imports: [CommonModule],
 })
-export class GestionarSolicitudComponent  implements OnInit {
+export class GestionarSolicitudComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  requests: Request[] = [
+    { name: 'Justin Septimus', email: 'example@email.com', status: 'Entregado', role: 'Tecnico', relatedTeam: 'Nombre artículo' },
+    { name: 'Anika Rhiel Madsen', email: 'example@email.com', status: 'Solicitado', role: 'Tecnico', relatedTeam: 'Nombre artículo' },
+    // Agrega más datos según sea necesario
+  ];
 }
+
+
