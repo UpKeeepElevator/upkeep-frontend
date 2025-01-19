bcd90cd392df2ba60ccf81cdc461e3ec0cceff16
Josmar <josmarreyes9875@gmail.com>
2025-01-19 00:33:15 -0400

    Gestionar averia, usuario(bug)

--- src/app/pages/admin/home-admin/home-admin.page.ts
+++ src/app/pages/admin/home-admin/home-admin.page.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { NavbarComponent } from "../../../shared/components/sidebar/sidebar.component";
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.page.html',
   styleUrls: ['./home-admin.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent, RouterOutlet, RouterLink]
+  imports: [CommonModule, FormsModule, NavbarComponent, RouterOutlet]
 })
 export class HomeAdminPage implements OnInit {
 
