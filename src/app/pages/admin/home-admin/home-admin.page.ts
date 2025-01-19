800985a4baff0e9e6a7798e979c75bc1ade6ffc9
shinso-raven <1116252@est.intec.edu.do>
2025-01-19 01:05:07 -0400

    fix: Tamaño gestionar

--- src/app/pages/admin/home-admin/home-admin.page.ts
+++ src/app/pages/admin/home-admin/home-admin.page.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from "../../../shared/components/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { IonIcon } from '@ionic/angular/standalone';
+import { NavbarComponent } from '../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.page.html',
   styleUrls: ['./home-admin.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, NavbarComponent, RouterOutlet]
+  imports: [IonIcon, CommonModule, FormsModule, NavbarComponent, RouterOutlet],
 })
 export class HomeAdminPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
